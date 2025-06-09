11ba48bb1891d2b9ef8ea4ce1c6046dc57478e52
Victor <victorantonio.vats1@gmail.com>
2025-06-09 16:23:24 -0300

    Criação de tabelas e primeiras funções DB

---
+++ Banco De dados/db.py
@@ -0,0 +1,86 @@
+# Banco de Dados Urna
+
+# Importando SQLite3
+import sqlite3
+
+# Conectando e criando o Banco de Dados ao código
+connection = sqlite3.connect("urna_futuro.db")
+
+# Cria um cursor que executará códigos SQL
+cursor = connection.cursor()
+
+# Manipulando dados com SQL
+
+# Criando Tabelas
+
+# Tabela de Tipos de Usuário
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS tipo_usuario (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        tipo TEXT
+    )
+''')
+
+# Tabela de Eleitores
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS eleitores (
+        cpf TEXT PRIMARY KEY,
+        nome TEXT,
+        rg TEXT,
+        data_nasc DATE,
+        FOREIGN KEY (tipo) REFERENCES tipo_usuario(id)  
+    )
+''')
+
+# Tabela de Candidatos
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS candidatos (
+        numero INTEGER PRIMARY KEY,
+        nome TEXT,
+        partido TEXT,
+        slogan TEXT,
+        proposta TEXT     
+    )
+''')
+
+# Tabela de Votos
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS votos (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        FOREIGN KEY (eleitor) REFERENCES eleitores(cpf),
+        FOREIGN KEY (candidato) REFERENCES candidatos(numero)
+    )
+''')
+
+# Funções para facilitar visualizações no Front-End
+
+def ver_votos_function():
+    # Cria a View
+    cursor.execute('''
+    CREATE VIEW votos_por_candidato AS
+        SELECT 
+            c.numero,
+            c.nome,
+            c.partido,
+            COUNT(v.id) AS total_votos,
+            ROUND(
+                100.0 * COUNT(v.id) / (
+                    SELECT COUNT(*) FROM votos
+                ), 2
+            ) AS porcentagem_votos
+        FROM candidatos c
+        LEFT JOIN votos v ON c.numero = v.candidato
+        GROUP BY c.numero, c.nome, c.partido;
+''')
+    # Executa a View
+    cursor.execute('''
+    SELECT 
+        candidato,
+        total_votos
+    FROM ver_votos
+''').fetchall()
+    
+def ver_candidatos():
+    cursor.execute('''
+    SELECT * FROM candidatos
+''').fetchall()
